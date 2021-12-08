67c2c78e9dc2c154ba64b86652db7dff2c59a7a5
t0t <sergiofores@gmail.com>
2021-12-08 22:40:46 +0100

    nuevo svg map

---
+++ public/obra.ff1983a5.js
@@ -0,0 +1,2 @@
+var myVideo=document.getElementById("video1");function playPause(){myVideo.paused?myVideo.play():myVideo.pause()}function makeBig(){myVideo.width=560}function makeSmall(){myVideo.width=320}function makeNormal(){myVideo.width=420}
+//# sourceMappingURL=obra.ff1983a5.js.map
