e040effa782e8dfb9dd136debc55a58967b30052
t0t <sergiofores@gmail.com>
2022-06-22 05:31:32 +0200

    cambio a vite

--- public/obra.ff1983a5.js
+++
@@ -1,2 +0,0 @@
-var myVideo=document.getElementById("video1");function playPause(){myVideo.paused?myVideo.play():myVideo.pause()}function makeBig(){myVideo.width=560}function makeSmall(){myVideo.width=320}function makeNormal(){myVideo.width=420}
-//# sourceMappingURL=obra.ff1983a5.js.map
