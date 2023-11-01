7f95339933ab28f9b8df0e1b96ef6edfae32ae48
sergio <sergiofores@gmail.com>
2023-11-01 19:35:10 +0100

    cambios y video

--- js/anims.js
+++ js/anims.js
@@ -17,36 +17,37 @@ animCrop.fromTo('.animCrop', {
   opacity: 1,
   width: "100vw",
   height: "100vh",
-  clipPath: "polygon(50% 0%, 100% 50%, 50% 100%, 0% 50%)",
+  clipPath: "inset(0% 0% 0% 0%)",
 },{
   opacity: 1,
   y: "40vh",
   x: "0vw",
-  clipPath: "polygon(100% 0%, 100% 50%, 50% 50%, 0% 50%)",
+  clipPath: "inset(50% 30% 100% 30%)",
   // mixBlendMode: "normal",
   // backgroundPosition: "top left"
 })
 
 // Anim crop 2
-let animCrop2 = gsap.timeline({
-  scrollTrigger: {
-    trigger: ".animCrop2",
-    start: "top 0vh",
-    end: "center 50vh",
-    scrub: true,
-    toggleActions: 'play play play reverse'
-  }
-})
-animCrop2.fromTo('.animCrop2', {
-  opacity: 1,
-  width: "100vw",
-  height: "100vh",
-  clipPath: "polygon(50% 0%, 100% 50%, 50% 100%, 0% 50%)",
-},{
-  opacity: 0.7,
-  y: "50vh",
-  x: "-10vw",
-  clipPath: "polygon(0% 0%, 75% 50%, 0% 100%, 0% 100%)",
-  // mixBlendMode: "normal",
-  // backgroundPosition: "top left"
-})
+// let animCrop2 = gsap.timeline({
+//   scrollTrigger: {
+//     trigger: ".animCrop2",
+//     start: "top 0vh",
+//     end: "center 50vh",
+//     scrub: true,
+//     toggleActions: 'play play play reverse'
+//   }
+// })
+// animCrop2.fromTo('.animCrop2', {
+//   opacity: 1,
+//   width: "100vw",
+//   height: "100vh",
+//   clipPath: "circle(40% at 50% 50%)",
+//   // clipPath: "polygon(50% 0%, 100% 50%, 50% 100%, 0% 50%)",
+// },{
+//   opacity: 0.7,
+//   y: "40vh",
+//   x: "-20vw",
+//   clipPath: "circle(20% at 50% 50%)",
+//   // mixBlendMode: "normal",
+//   // backgroundPosition: "top left"
+// })
