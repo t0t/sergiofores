577a0e6eb86b22122a3c30b991e64671609f70e1
sergio <sergiofores@gmail.com>
2023-11-03 16:12:39 +0100

    cambio look

--- js/main.js
+++ js/main.js
@@ -15,6 +15,7 @@ window.addEventListener("load", () =>  {
   // Muestra la pagina
   theSite.style.display = "inherit";
   noOverflow.style.overflow = "visible";
+  
 });
 
 let fundidoPagina = () => {
@@ -28,18 +29,18 @@ anime({
   targets: '.titlePage',
   // scale: ['3', '1.5'],
   keyframes: [
-    {scale: 2},
-    {translateY: -300},
-    {translateX: 250},
-    {translateY: 40},
+    {scale: .8},
+    {rotate: '-10'},
     {translateX: 0},
     {translateY: 0},
-    {scale: 1}
-    // {rotate: '-10deg'}
+    {scale: 1},
+    {rotate: '0'},
+    {rotate: '10'}
   ],
-  rotate: '1turn',
+  easing: 'easeInOutSine',
   // backgroundColor: '#000',
-  duration: 3500
+  duration: 3500,
+  loop: true
   // borderRadius: ['0%', '5%']
 });
 
@@ -53,3 +54,49 @@ anime({
 // let credits = () => {
 //   console.log("Website desarrollada a mano y con mucho cariño por Sergio Forés")
 // }
+
+
+
+
+function eventoDragVentanas(){
+  const VENTANAS = document.querySelectorAll(".drag");
+
+  VENTANAS.forEach(ventana => {
+          
+      let mousePosition   = [0,0];
+      let element         = document.getElementById(ventana.id);
+      let elementPosition = [10 , 55];
+      
+      let mainEH = function (event) {
+              let rect = event.target.getBoundingClientRect();
+              elementPosition = [rect.left , rect.top];
+              mousePosition = [event.clientX, event.clientY];
+              document.addEventListener('mousemove', calcEH);
+              document.addEventListener('mouseup', removeHandlers);
+              document.addEventListener('contextmenu', removeHandlers);
+      };
+      
+      let calcEH = function (event) {
+          let vector      = [-mousePosition[0] + event.clientX, -mousePosition[1] + event.clientY];
+          mousePosition   = [mousePosition[0] + vector[0], mousePosition[1] + vector[1]];
+          elementPosition = [elementPosition[0] + vector[0], elementPosition[1] + vector[1]];
+          updatePosition();
+      };
+      
+      let removeHandlers = function () {
+          document.removeEventListener('mousemove', calcEH);
+          document.removeEventListener('mouseup', removeHandlers);
+          document.removeEventListener('contextmenu', removeHandlers);
+      };
+      
+      function updatePosition() {
+          element.style.left = elementPosition[0] + "px";
+          element.style.top  = elementPosition[1] + "px";
+      }
+      
+      element.addEventListener('mousedown', mainEH, true);
+          
+          
+  });
+}
+eventoDragVentanas();
