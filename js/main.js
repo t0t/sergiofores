fc20c057911e0bf8f13de6e1e1d8aad1bf51604f
sergio <sergiofores@gmail.com>
2023-11-07 17:20:42 +0100

    cambios

--- js/main.js
+++ js/main.js
@@ -26,21 +26,22 @@ let fundidoPagina = () => {
 import anime from 'animejs/lib/anime.es.js';
 
 anime({
-  targets: '.titlePage',
+  targets: '.animeintro',
+  opacity: '1',
   // scale: ['3', '1.5'],
   keyframes: [
     {scale: .8},
     {rotate: '-10'},
-    {translateX: 0},
-    {translateY: 0},
+    {translateX: 1},
+    {translateY: 1},
     {scale: 1},
+    {rotate: '10'},
     {rotate: '0'},
-    {rotate: '10'}
   ],
   easing: 'easeInOutSine',
   // backgroundColor: '#000',
-  duration: 3500,
-  loop: true
+  duration: 1500
+  // loop: true
   // borderRadius: ['0%', '5%']
 });
 
