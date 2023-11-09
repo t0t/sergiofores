a87b2c3a1a6ee1b1693c0022b1d7f9dac1ee9eb5
sergio <sergiofores@gmail.com>
2023-11-09 20:12:04 +0100

    refactoring

--- js/main.js
+++ js/main.js
@@ -123,17 +123,21 @@ document.querySelectorAll('.draggable').DraggableJS();
 
 // SLIDESHOW
 // https://www.w3schools.com/w3css/w3css_slideshow.asp
-var slideIndex = [1, 1, 1, 1];
+var slideIndex = [1, 1, 1, 1, 1, 1];
 var slideId = [
   "slider1",
   "slider2",
   "slider3",
-  "slider4"
+  "slider4",
+  "slider5",
+  "slider6"
 ]
 showDivs(1, 0);
 showDivs(1, 1);
 showDivs(1, 2);
 showDivs(1, 3);
+showDivs(1, 4);
+showDivs(1, 5);
 
 // main.js is added as a type=module. Module creates a scope to avoid name collisions.
 // You can either expose your function to the window object
