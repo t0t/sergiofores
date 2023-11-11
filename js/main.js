9fa954e07427f89a675087682727d9e80125e954
sergio <sergiofores@gmail.com>
2023-11-11 17:48:11 +0100

    prepross

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
-import '../sass/style.scss'
+// import '../sass/style.scss'
+
 // import { plusDivs, showDivs } from './slider.js'
 // import { slideIndex, slideId } from './slider.js'
+
 
 // LOADER con fundido
 let loader = document.getElementById("wrap-preloader");
@@ -9,7 +11,7 @@ let noOverflow = document.querySelector("html");
 
 
 function cargadoDOM() {
-  // console.info("DOM cargado!");
+  console.info("DOM cargado!");
   loader.style.display = "flex";
 }
 
@@ -25,7 +27,7 @@ if (document.readyState === "loading") {
 
 // Cuando carge todo,  DOM, recursos, etc
 window.addEventListener("load", () => {
-  // console.info("Recursos cargados!");
+  console.info("Recursos cargados!");
   loader.style.display = "none";
   theSite.style.display = "block";
   noOverflow.style.overflow = "auto";
@@ -36,32 +38,74 @@ let fundidoPagina = () => {
   document.querySelector("#theSite").classList.add("fade-page-on")
 }
 
-// ANIME 
-import anime from 'animejs/lib/anime.es.js';
 
-anime({
-  targets: '.animeintro',
-  // opacity: '1',
-  scale: ['3', '1.5', '1', '.9'],
-  keyframes: [
-    { rotate: '-10' },
-    { translateX: "10" },
-    { translateY: "-10" },
-    { rotate: '10' },
-    { rotate: '0' },
-    { translateX: "0" },
-    { translateY: "0" },
-    { scale: "1" },
-    { rotateY: "60deg" },
-    { perspective: "10vw" },
-    { perspective: "100vw" }
-  ],
-  easing: 'easeInOutSine',
-  // backgroundColor: '#000',
-  duration: 1500
-  // loop: true
-  // borderRadius: ['0%', '5%']
-});
+// SLIDESHOW
+// https://www.w3schools.com/w3css/w3css_slideshow.asp
+var slideIndex = [1, 1, 1, 1, 1, 1];
+var slideId = [
+  "slider1",
+  "slider2",
+  "slider3",
+  "slider4",
+  "slider5",
+  "slider6"
+]
+showDivs(1, 0);
+showDivs(1, 1);
+showDivs(1, 2);
+showDivs(1, 3);
+showDivs(1, 4);
+showDivs(1, 5);
+
+// main.js is added as a type=module. Module creates a scope to avoid name collisions.
+// You can either expose your function to the window object
+window.plusDivs = plusDivs
+
+function plusDivs(n, no) {
+  showDivs(slideIndex[no] += n, no);
+}
+
+function showDivs(n, no) {
+  var i;
+  var x = document.getElementsByClassName(slideId[no]);
+
+  if (n > x.length) { slideIndex[no] = 1 }
+  if (n < 1) { slideIndex[no] = x.length }
+
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+
+  x[slideIndex[no] - 1].style.display = "block";
+
+}
+
+// ANIME 
+// import anime from 'animejs/lib/anime.es.js';
+
+// anime({
+//   targets: '.animeintro',
+//   // opacity: '1',
+//   scale: ['3', '1.5', '1', '.9'],
+//   keyframes: [
+//     { rotate: '-10' },
+//     { translateX: "10" },
+//     { translateY: "-10" },
+//     { rotate: '10' },
+//     { rotate: '0' },
+//     { translateX: "0" },
+//     { translateY: "0" },
+//     { scale: "1" },
+//     { rotateY: "60deg" },
+//     { perspective: "10vw" },
+//     { perspective: "100vw" }
+//   ],
+//   easing: 'easeInOutSine',
+//   // backgroundColor: '#000',
+//   duration: 1500
+//   // loop: true
+//   // borderRadius: ['0%', '5%']
+// });
 
 
 
@@ -108,43 +152,3 @@ window.addEventListener('resize', function () {
 });
 
 
-// SLIDESHOW
-// https://www.w3schools.com/w3css/w3css_slideshow.asp
-var slideIndex = [1, 1, 1, 1, 1, 1];
-var slideId = [
-  "slider1",
-  "slider2",
-  "slider3",
-  "slider4",
-  "slider5",
-  "slider6"
-]
-showDivs(1, 0);
-showDivs(1, 1);
-showDivs(1, 2);
-showDivs(1, 3);
-showDivs(1, 4);
-showDivs(1, 5);
-
-// main.js is added as a type=module. Module creates a scope to avoid name collisions.
-// You can either expose your function to the window object
-window.plusDivs = plusDivs
-
-function plusDivs(n, no) {
-  showDivs(slideIndex[no] += n, no);
-}
-
-function showDivs(n, no) {
-  var i;
-  var x = document.getElementsByClassName(slideId[no]);
-
-  if (n > x.length) { slideIndex[no] = 1 }
-  if (n < 1) { slideIndex[no] = x.length }
-
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
-
-  x[slideIndex[no] - 1].style.display = "block";
-
-}
